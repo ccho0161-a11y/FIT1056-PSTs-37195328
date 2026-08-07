1fc9754bc22032516fd108aeb5160cb638d14efd
Claire Chong <ccho0161@student.monash.edu>
2026-08-07 12:46:31 +1000

    chore: add .gitkeep to empty folders

---
+++ practice_programs/Centimeter to inch converter.py
@@ -0,0 +1,4 @@
+#centimeter to inch converter
+centimeter = float(input("how many cm: "))
+inch = 2.54*centimeter
+print(inch)
---
+++ practice_programs/Speed calculator.py
@@ -0,0 +1,4 @@
+dist = float(input("How far did u go in km: "))
+duration = float(input("How long in mins: "))
+speed = dist/(duration/60)
+print(f"The average speed was {speed}km/h")
